c6508c2e80a406d79ff9dd564ccfb548577e0573
Ken Ramandha <93147426+KenRamandha@users.noreply.github.com>
2025-10-19 00:34:38 +0700

    commit

--- app/Models/Core/Permission.php
+++ app/Models/Core/Permission.php
@@ -2,7 +2,7 @@
 
 namespace App\Models\Core;
 
-use Illuminate\Database\Eloquent\Factories.HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
--- app/Models/Menus/Menu.php
+++ app/Models/Menus/Menu.php
@@ -2,7 +2,7 @@
 
 namespace App\Models\Menus;
 
-use Illuminate\Database\Eloquent\Factories.HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
